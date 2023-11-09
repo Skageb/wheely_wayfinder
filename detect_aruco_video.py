8d8e83f1cf44887516ef75dcc20e89c146cb1aea
Skageb <skagegb@hotmail.no>
2023-11-09 13:33:52 +0100

    Initial commit with code for detection and handling of aruco markers

---
+++ detect_aruco_video.py
@@ -0,0 +1,53 @@
+import cv2
+import cv2.aruco as aruco
+from marker_handler import MarkerHandlers
+import inspect
+
+# Initialize the video capture from the first camera device
+cap = cv2.VideoCapture(0)
+
+
+# Get the predefined dictionary
+arucoDict = aruco.getPredefinedDictionary(aruco.DICT_5X5_50)
+
+detector = aruco.ArucoDetector(arucoDict)
+
+#Load marker handlers:
+mh = MarkerHandlers()
+handlerFunctions = [func for name, func in inspect.getmembers(mh, inspect.ismethod)]
+current_id = 0
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to grab frame")
+        break
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect the markers in the image using the ArucoDetector object
+    corners, ids, rejectedImgPoints = detector.detectMarkers(image=gray)
+
+    # Check if markers are detected
+    if ids is not None:
+        # If markers are detected, overlay their ID and outline them in the frame
+        aruco.drawDetectedMarkers(frame, corners, ids)
+    #Checks if new id detected and executes corresponding handler function
+    if ids is not None:
+        id = ids[0][0]
+        if id != current_id:
+            handlerFunctions[id-1]()  
+            current_id = id
+    else:
+        #Set current_id to 0 current id is removed from frame
+        current_id = 0
+    # Display the resulting frame
+    cv2.imshow('Frame', frame)
+    # Break the loop when 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture and destroy windows
+cap.release()
+cv2.destroyAllWindows()
---
+++ marker_handler.py
@@ -0,0 +1,15 @@
+class MarkerHandlers:
+    def handle_marker_1(self):
+        print("Marker 1 detected and handled. *Jetson moves forward*")
+
+    def handle_marker_2(self):
+        print("Marker 2 detected and handled. *Jetson turns right*")
+
+    def handle_marker_3(self):
+        print("Marker 3 detected and handled. *Jetson turns left*")
+
+    def handle_marker_4(self):
+        print("Marker 4 detected and handled. *Jetson moves backward*")
+
+    def handle_marker_5(self):
+        print("Marker 5 detected and handled. *Jetson stops moving*")
